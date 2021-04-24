a7e9145abf0a14d8e4df28e68c75480afbc10058
Josip <josip8grga@gmail.com>
2021-04-25 01:29:33 +0200

    Add migration - all other tables

---
+++ src/data/Migrations/20210424232812_BettingTables.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace data.Migrations
+{
+    public partial class BettingTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "AspNetUsers",
+                newName: "WalletAmount");
+
+            migrationBuilder.CreateTable(
+                name: "Sport",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sport", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tip",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TipName = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tip", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transaction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    OldAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    NewAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    TransactionType = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transaction_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SportTeam",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    SportId = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SportTeam", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SportTeam_Sport_SportId",
+                        column: x => x.SportId,
+                        principalTable: "Sport",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pair",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SpecialOffer = table.Column<bool>(type: "boolean", nullable: false),
+                    SpecialOfferModifier = table.Column<float>(type: "real", nullable: false),
+                    HomeTeamId = table.Column<int>(type: "integer", nullable: false),
+                    AwayTeamId = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pair", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pair_SportTeam_AwayTeamId",
+                        column: x => x.AwayTeamId,
+                        principalTable: "SportTeam",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pair_SportTeam_HomeTeamId",
+                        column: x => x.HomeTeamId,
+                        principalTable: "SportTeam",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SportTip",
+                columns: table => new
+                {
+                    SportId = table.Column<int>(type: "integer", nullable: false),
+                    TipId = table.Column<int>(type: "integer", nullable: false),
+                    SportTeamId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SportTip", x => new { x.SportId, x.TipId });
+                    table.ForeignKey(
+                        name: "FK_SportTip_Sport_SportId",
+                        column: x => x.SportId,
+                        principalTable: "Sport",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SportTip_SportTeam_SportTeamId",
+                        column: x => x.SportTeamId,
+                        principalTable: "SportTeam",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SportTip_Tip_TipId",
+                        column: x => x.TipId,
+                        principalTable: "Tip",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PairTip",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PairId = table.Column<int>(type: "integer", nullable: false),
+                    TipId = table.Column<int>(type: "integer", nullable: false),
+                    Coefficient = table.Column<float>(type: "real", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PairTip", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PairTip_Pair_PairId",
+                        column: x => x.PairId,
+                        principalTable: "Pair",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PairTip_Tip_TipId",
+                        column: x => x.TipId,
+                        principalTable: "Tip",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ticket",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    PrizeAmount = table.Column<int>(type: "integer", nullable: false),
+                    Coefficient = table.Column<int>(type: "integer", nullable: false),
+                    ManipulativeCosts = table.Column<decimal>(type: "numeric", nullable: false),
+                    SpecialOfferPairId = table.Column<int>(type: "integer", nullable: true),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticket", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ticket_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ticket_Pair_SpecialOfferPairId",
+                        column: x => x.SpecialOfferPairId,
+                        principalTable: "Pair",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TicketPairTip",
+                columns: table => new
+                {
+                    TicketId = table.Column<int>(type: "integer", nullable: false),
+                    PairTipId = table.Column<int>(type: "integer", nullable: false),
+                    PairId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketPairTip", x => new { x.TicketId, x.PairTipId });
+                    table.ForeignKey(
+                        name: "FK_TicketPairTip_Pair_PairId",
+                        column: x => x.PairId,
+                        principalTable: "Pair",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TicketPairTip_PairTip_PairTipId",
+                        column: x => x.PairTipId,
+                        principalTable: "PairTip",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TicketPairTip_Ticket_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Ticket",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pair_AwayTeamId",
+                table: "Pair",
+                column: "AwayTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pair_HomeTeamId",
+                table: "Pair",
+                column: "HomeTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PairTip_PairId",
+                table: "PairTip",
+                column: "PairId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PairTip_TipId",
+                table: "PairTip",
+                column: "TipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SportTeam_SportId",
+                table: "SportTeam",
+                column: "SportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SportTip_SportTeamId",
+                table: "SportTip",
+                column: "SportTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SportTip_TipId",
+                table: "SportTip",
+                column: "TipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_SpecialOfferPairId",
+                table: "Ticket",
+                column: "SpecialOfferPairId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_UserId",
+                table: "Ticket",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketPairTip_PairId",
+                table: "TicketPairTip",
+                column: "PairId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketPairTip_PairTipId",
+                table: "TicketPairTip",
+                column: "PairTipId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaction_UserId",
+                table: "Transaction",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SportTip");
+
+            migrationBuilder.DropTable(
+                name: "TicketPairTip");
+
+            migrationBuilder.DropTable(
+                name: "Transaction");
+
+            migrationBuilder.DropTable(
+                name: "PairTip");
+
+            migrationBuilder.DropTable(
+                name: "Ticket");
+
+            migrationBuilder.DropTable(
+                name: "Tip");
+
+            migrationBuilder.DropTable(
+                name: "Pair");
+
+            migrationBuilder.DropTable(
+                name: "SportTeam");
+
+            migrationBuilder.DropTable(
+                name: "Sport");
+
+            migrationBuilder.RenameColumn(
+                name: "WalletAmount",
+                table: "AspNetUsers",
+                newName: "Amount");
+        }
+    }
+}
