aae331206cd83fb758def3d5e72858133288aaa2
Josip <josip8grga@gmail.com>
2021-04-26 22:29:14 +0200

    Pair controller

---
+++ src/server/Controllers/PairController.cs
@@ -0,0 +1,46 @@
+﻿using data.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Controllers
+{
+  [Route("api/[controller]/[action]")]
+  [ApiController]
+  public class PairController : ControllerBase
+  {
+    private readonly BettingAppDbContext _context;
+    public PairController(BettingAppDbContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult AllActivePairs()
+    {
+      var pairs = _context.Pair.Where(x => x.GameStart > DateTime.Now)
+        .Include(x => x.PairTips)
+        .Include(x => x.HomeTeam)
+        .Include(x => x.AwayTeam);
+
+      return Ok(pairs.Select(x => new
+      {
+        HomeTeam = x.HomeTeam.Name,
+        AwayTeam = x.AwayTeam.Name,
+        x.Id,
+        x.GameStart,
+        PairTips = x.PairTips.Select(pt => new
+        {
+          pt.Id,
+          pt.Coefficient,
+          pt.Status,
+          pt.Tip.TipName
+        })
+      }));
+    }
+  }
+}
--- src/server/Startup.cs
+++ src/server/Startup.cs
@@ -36,6 +36,8 @@ namespace server
       services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<BettingAppDbContext>().AddDefaultTokenProviders();
 
       services.AddControllers();
+      // .AddNewtonsoftJson(options =>options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "server", Version = "v1" });
